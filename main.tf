260620aee35fa8a820d630127b7efd2c4e8dc5e1
ricardodias <rigoncalvesdias@gmail.com>
2021-01-19 22:52:57 -0300

    Alterando o Projeto do GCP

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+#### SETUP GOOGLE ####
 terraform {
   required_providers {
     google = {
@@ -10,19 +11,20 @@ terraform {
 provider "google" {
   version = "3.5.0"
 
-  
-  project = "ricardodias-mbaimpacta-turma04"
+  project = "ricardodias-cloudautomation" 
   region  = "us-central1"
   zone    = "us-central1-c"
-
 }
+
+
+#### RECURSOS NO GCP #####
 
 # Cria uma VM no Google Cloud
 resource "google_compute_instance" "firstvm" {
-  name         = "helloworld"
+  name         = "website"
   machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
-  tags         = ["website", "impacta"]
+  zone         = "us-central1-c"
+  tags         = ["website"]
 
   # Defini a Imagem da VM
   boot_disk {
@@ -35,10 +37,9 @@ resource "google_compute_instance" "firstvm" {
   network_interface {
     network = "default" # Estamos usando a VPC default que já vem por padrão no projeto.
 
-
     access_config {
-      // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
     }
+    // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
   }
 }
 
